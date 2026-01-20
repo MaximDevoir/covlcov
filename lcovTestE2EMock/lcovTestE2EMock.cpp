536d8391feba463c9aee66635ee1326527d86729
Maxim Devoir <maximdevoir@gmail.com>
2026-01-20 00:30:01 -0800

    add mock test

---
+++ lcovTestE2EMock/lcovTestE2EMock.cpp
@@ -0,0 +1,19 @@
+int Add(int a, int b) {
+	return a + b;
+}
+
+int Subtract(int a, int b) {
+	return a - b;
+}
+
+int Multiply(int a, int b) {
+	return a * b;
+}
+
+
+int main(int argc, char* argv[]) {
+	Add(1, 3);
+	Multiply(4, 2);
+	return 0;
+	return 0;
+}
